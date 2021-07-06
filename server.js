69164d164cdd3b79e0381ce70948303959b0238b
nasernikzad <n.nikzad@pomtech.com>
2021-07-06 14:01:02 +0430

    changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+var multer  = require('multer');
+var upload = multer()
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +13,19 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'), function (req, res, next) {
 
+  var info = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  }
 
+  res.json(info);
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+})
+
+// Start server and print port.
+const listener = app.listen(process.env.PORT, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
